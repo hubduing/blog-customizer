830339906382f6b914afd17df23800df6c0e8035
Alexey Borisov <UralMana@yandex.ru>
2024-05-27 12:28:07 +0500

    Add open form

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,34 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { useEffect, useRef } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
-
-export const ArrowButton = () => {
+export type OnClick = {
+	callBack: () => void;
+};
+export const ArrowButton: React.FC<OnClick> = ({ callBack }) => {
+	const arrowButtonRef = useRef<HTMLDivElement | null>(null);
+	const arrowImgRef = useRef<HTMLImageElement | null>(null);
+	useEffect(() => {
+		arrowButtonRef.current?.classList.toggle(styles.container_open);
+		arrowImgRef.current?.classList.toggle(styles.arrow_open);
+	}, [callBack]);
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			ref={arrowButtonRef}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={styles.container}
+			onClick={callBack}>
+			<img
+				ref={arrowImgRef}
+				src={arrow}
+				alt='иконка стрелочки'
+				className={styles.arrow}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,29 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm: React.FC = () => {
+	const asideRef = useRef<HTMLElement | null>(null);
+	const [open, setOpen] = useState(false);
+
+	const arrowButtonHandler = useCallback(() => {
+		setOpen(!open);
+	}, [open]);
+
+	useEffect(() => {
+		if (!open) {
+			console.log('CLOSE', asideRef.current);
+			asideRef.current?.classList.remove(styles.container_open);
+		} else {
+			console.log('OPEN', asideRef.current);
+			asideRef.current?.classList.add(styles.container_open);
+		}
+	}, [open]);
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton callBack={arrowButtonHandler} />
+			<aside ref={asideRef} className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
